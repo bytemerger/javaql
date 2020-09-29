b663c27ca57ea6218b05e5dd655cc2daed426ad8
franc <onyishif@gmail.com>
2020-09-29 23:53:56 +0100

    add query resolver

---
+++ src/main/java/com/api/graphql/resolver/Query.java
@@ -0,0 +1,31 @@
+package com.api.graphql.resolver;
+
+import com.api.graphql.entity.Dog;
+import com.api.graphql.exceptions.DogNotFoundException;
+import com.api.graphql.repository.DogRepository;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+    private DogRepository dogRepository;
+
+    public Query(DogRepository dogRepository) {
+        this.dogRepository = dogRepository;
+    }
+
+    public Iterable<Dog> findAllDogs(){
+        return dogRepository.findAll();
+    }
+    public Dog findDogById(Long id) {
+        Optional<Dog> optionalDog = dogRepository.findById(id);
+        if (optionalDog.isPresent()) {
+            return optionalDog.get();
+        } else {
+            throw new DogNotFoundException("Dog Not Found", id);
+        }
+    }
+}
